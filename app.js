17d733baec42c8fd7a291d2ead045ea404eca2c7
Danilo Heraclio <emaildedanilo@gmail.com>
2019-03-10 15:28:27 -0400

    created a Game Initialization function

--- app.js
+++ app.js
@@ -9,18 +9,7 @@ GAME RULES
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-// start game without showing the dice
-document.querySelector('.dice').style.display = 'none';
-
-// setting the initial values
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 //add event listener to roll button using click event and anonymous function
 document.querySelector('.btn-roll').addEventListener('click', () => {
@@ -94,6 +83,31 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 }
 
-function gameOver() {
+document.querySelector('.btn-new').addEventListener('click', init);
 
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  // start game without showing the dice
+  document.querySelector('.dice').style.display = 'none';
+
+  // setting the initial values
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
 }
